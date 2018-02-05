9d1cedb2800d1051b60f0fb5891abf1c8e892c26
Sam Marder <smarder@google.com>
2018-02-05 11:18:16 -0500

    Fix typo in string interpoliation in group.rb

--- lib/accredible-api-ruby/group.rb
+++ lib/accredible-api-ruby/group.rb
@@ -25,7 +25,7 @@ module Accredible
 
     def self.view(group_id = nil)
       uri = Group.api_end_point(group_id)
-      Accredible.request(uri) 
+      Accredible.request(uri)
     end
 
     def self.api_end_point(id = nil)
@@ -34,8 +34,7 @@ module Accredible
 
 
     def self.view_all_end_point(page,page_size)
-      Accredible.api_url("/issuer/all_groups?page=#{page}&page_size={page_size}")
+      Accredible.api_url("/issuer/all_groups?page=#{page}&page_size=#{page_size}")
     end
   end
 end
-
